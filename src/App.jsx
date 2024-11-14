5fa373c8437dbd33f59532f3e881bc77fba9ce58
masum <ahmedmasum.b97@gmail.com>
2024-11-14 12:26:24 +0600

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,127 @@
+import { useState } from 'react'
 import './App.css'
 import BioDataTwo from './component/biodata2'
 function App() {
 
   function Item({name,isPacked}){
 
-    const isCheckMark = isPacked? 'ok' : null
+    // const isCheckMark = isPacked? 'ok' : null
+    // logical or
+    const user = name || 'Ahmed Masum'
+
+    // nalish qualicing 
+
+    let use1 = name ?? "Ahmed Masum"
+
+    // both nalish qualicing and logical or use for default value
 
     return (
+    
       // <li>{name} {isPacked? "ok":null}</li>
-      <li>{name} {isCheckMark}</li>
+      // <li>{name} {isCheckMark}</li>
+      <li>{name} {isPacked && 'ok'}</li>
     )
 
   }
 
+
+  // let content;
+  // if(!user) content = 'Guest';
+  // if (user && isAdmin) content = 'Admin';
+
+
+  const people = [
+    'Creola Katherine Johnson: mathematician',
+    'Mario José Molina-Pasquel Henríquez: chemist',
+    'Mohammad Abdus Salam: physicist',
+    'Percy Lavon Julian: chemist',
+    'Subrahmanyan Chandrasekhar: astrophysicist'
+  ];
+
+
+
+  const person = [{
+    id: 0,
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+  }, {
+    id: 1,
+    name: 'Mario José Molina-Pasquel Henríquez',
+    profession: 'chemist',
+  }, {
+    id: 2,
+    name: 'Mohammad Abdus Salam',
+    profession: 'physicist',
+  }, {
+    id: 3,
+    name: 'Percy Lavon Julian',
+    profession: 'chemist',  
+  }, {
+    id: 4,
+    name: 'Subrahmanyan Chandrasekhar',
+    profession: 'astrophysicist',
+  }];
+
+  const chemists = person.filter( (chemist) => chemist.profession === "chemist")
+
+
+
+// useState function
+
+const [count,setCount] = useState(0)
+
+const handleClick = () => {
+
+  setCount(count+1)
+ 
+}
+
+
  
 
   return (
+
+    
+
    
 
       <div>
+
+        <div>
+          <h1 style={{ backgroundColor:"red",color:'white',padding:'10px', width:"100px",textAlign:'center',borderRadius:'10px',boxShadow:'2px 3px 2px 2px blue',margin:'10px auto'}}>{count}</h1>
+          <button type='button' style={{ padding:'10px',borderRadius:'10px',margin:'10px auto',display:'block' , backgroundColor:'blue',color:'white'}} onClick={handleClick}>Increment</button>
+        </div>
+
+        <div>
+          <h1>List of people</h1>
+          <ul>
+            { people.map( (person,index) => {
+             return <li key={index}>{person}</li>
+            })}
+          </ul>
+        </div>
+        
+
+
+      <div>
+        <h1>Person about</h1>
+          <ul>
+            { 
+               person.map( (person) => {
+                return <li key={person.id}>{person.name}</li>
+               })
+            }
+          </ul>
+
+          <h1>Chemist name</h1>
+          <ul>
+            {
+              chemists.map( (chemist) => {
+                return <li key={chemist.id}>{chemist.name}</li>
+              })
+            }
+          </ul>
+      </div>
 
         <section>
           <h1>Bio Data</h1>
